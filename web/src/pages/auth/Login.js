790b90c3f30a3de3842781c83aa71a9a33081125
natewu <36091727+natewu@users.noreply.github.com>
2021-05-28 23:36:02 -0600

    login ui

--- web/src/pages/auth/Login.js
+++ web/src/pages/auth/Login.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
+import {Link} from "react-router-dom";
 import {TextField, Button} from "@material-ui/core";
 import {handleSignup, handleLogin} from "utils/auth.js";
+import "styles/Auth.scss";
 
 export default function Login() {
 
@@ -8,11 +10,20 @@ export default function Login() {
   const[password, setPassword] = useState(null);
 
   return (
-    <div id="auth-container">
-        <TextField label="email" type="email" size="small" onChange={e => setEmail(e.target.value)} fullWidth variant="outlined"/>
-        <TextField label="password" type="password" size="small" onChange={e => setPassword(e.target.value)} fullWidth variant="outlined"/>
-        <Button fullWidth onClick={() => handleLogin(email, password)}>Login</Button>
-        <Button fullWidth onClick={() => handleSignup(email, password)}>Signup</Button>
+    <div className="wrapper">
+      <div className="logo">
+        <h2>Strife</h2>
+      </div>
+      <div className="auth-container">
+        <h2>Welcome back!</h2>
+        <TextField className="input" label="email" type="email" size="small" onChange={e => setEmail(e.target.value)} fullWidth variant="filled" />
+        <TextField className="input" label="password" type="password" size="small" onChange={e => setPassword(e.target.value)} fullWidth variant="filled"/>
+        <span className="register input"><Link to="/signup" >Forgot your password?</Link> </span>
+        <Button className="input" fullWidth onClick={() => handleLogin(email, password)}>Login</Button>
+        {/* <Button className="input" fullWidth onClick={() => handleSignup(email, password)}>Signup</Button> */}
+        <span className="register">Need an account? <Link to="/signup" >Register</Link> </span>
+      </div>
+      
     </div>
   )
 }
