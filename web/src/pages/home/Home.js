902ff5832b263447be92421fa17eb4629aa9fb19
natewu <36091727+natewu@users.noreply.github.com>
2021-07-25 15:48:45 -0600

    new homepage ui experience

--- web/src/pages/home/Home.js
+++ web/src/pages/home/Home.js
@@ -1,11 +1,14 @@
 import React, {  } from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import "styles/Home.scss";
 
 export default function Home() {
    return (
-      <div>
-         Home - 
-         <Link to="/app">Launch app</Link>
+      <div className="home">
+         <div className="launch__app">
+            <h1 className="logo">STRIFE</h1>
+            <Link to="/app" className="launch__btn">Launch App</Link>
+         </div>
       </div>
    )
 }
